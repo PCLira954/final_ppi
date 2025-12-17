29011cd783312e3c894340209bf3b814c31ae7d4
Paulo César <126837594+PCLira954@users.noreply.github.com>
2025-12-17 09:13:17 -0300

    Delete lib directory

--- lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const db = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT),
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
